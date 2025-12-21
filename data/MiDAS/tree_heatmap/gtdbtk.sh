866818949991daebc8b483a1e4d5ce28c3aed41e
ZS85XK <zs85xk@bio.aau.dk@bio-node11.srv.aau.dk>
2025-12-21 10:48:55 +0100

    Added new figure 4 code, cleaned up for preprint publication

---
+++ data/MiDAS/tree_heatmap/gtdbtk.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash -l
+#SBATCH --job-name=gtdbtk_tree
+#SBATCH --output=/home/bio.aau.dk/zs85xk/projects/epsSMASH/epsSMASH_analyses/logs/job_%j_%x.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=high-mem
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=150G
+#SBATCH --time=0-06:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=aohd@bio.aau.dk
+
+# Exit on first error and if any variables are unset
+set -eu
+
+#Set working directory
+WD=/home/bio.aau.dk/zs85xk/projects/epsSMASH/epsSMASH_paper/data/MiDAS/tree_heatmap
+cd $WD
+
+# variables
+THREADS=$(nproc)
+ODIR=gtdbtk
+mkdir -p $ODIR
+
+GENOMES=genomes/
+
+mkdir -p $GENOMES
+
+cut -f 1 /home/bio.aau.dk/zs85xk/projects/epsSMASH/epsSMASH_paper/data/MiDAS/heatmap_tree_genomes.tsv | while read -r filename; do
+    echo $filename
+    cp /home/bio.aau.dk/zs85xk/projects/epsSMASH/epsSMASH_analyses/MiDAS/data/derep_HQ_MAGs/$filename.fa genomes/
+done
+
+# # activate conda env
+eval "$(conda shell.bash hook)"
+
+conda activate gtdbtk-2.4.1
+
+
+gtdbtk classify_wf --genome_dir $GENOMES \
+                    --out_dir $ODIR \
+                    --cpus $THREADS \
+                    -x fa --skip_ani_screen --debug
