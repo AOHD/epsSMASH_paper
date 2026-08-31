1cac10614f97d866260c1342eeb8a057611ac3f5
ZS85XK <zs85xk@bio.aau.dk@bio-node11.srv.aau.dk>
2026-08-31 12:15:05 +0200

    Trial commit for running all scripts

--- scripts/bigscape_MiDAS.sh
+++ scripts/bigscape_MiDAS.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash -l
-#SBATCH --job-name=bigscape_MiDAS_core_0.4
+#SBATCH --job-name=bigscape_MiDAS_global_0.4
 #SBATCH --output=/home/bio.aau.dk/zs85xk/projects/epsSMASH/bigscape/slurmlog/job_%j_%x.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=40
 #SBATCH --mem=300G
-#SBATCH --time=0-12:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=aohd@bio.aau.dk
 
@@ -17,4 +17,4 @@ conda activate bigscape
 
 cd /home/bio.aau.dk/zs85xk/projects/epsSMASH/
 
-bigscape cluster -i epsSMASH_paper/data/MiDAS/epsSMASH_results_10k_core/antismash -o epsSMASH_paper/results/bigscape/core/ -p bigscape/pfam/Pfam-A.hmm -c 20 --force-gbk --gcf-cutoffs 0.4
+bigscape cluster -i epsSMASH_paper/data/MiDAS/epsSMASH_results/antismash -o epsSMASH_paper/results/bigscape/MiDAS/ -p bigscape/pfam/Pfam-A.hmm -c 20 --force-gbk --gcf-cutoffs 0.4
